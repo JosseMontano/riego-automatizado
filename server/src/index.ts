9d2682153697144469e0f712f5ccbbd1cb00728a
JosseMontano <eljosema505@gmail.com>
2025-01-18 17:24:21 -0400

    feat: Change position the env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import authRoutes from './routes/authRoute';
-import sensorRoutes from './routes/sensorRoute';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoute";
+import sensorRoutes from "./routes/sensorRoute";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -11,23 +11,23 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI || "")
+mongoose
+  .connect(process.env.MONGO_URI || "")
   .then(() => {
-    console.log('Conectado a MongoDB');
+    console.log("Conectado a MongoDB");
   })
   .catch((error) => {
-    console.error('error al conectar con MongoDB: ', error);
-  });
+    console.error("error al conectar con MongoDB: ", error);
+});
 
-app.use('/auth', authRoutes);
-app.use('/api', sensorRoutes);
-app.use('/api', sensorRoutes);
+app.use("/auth", authRoutes);
+app.use("/api", sensorRoutes);
+app.use("/api", sensorRoutes);
 
-app.get('/', (_, res) => {
-  res.send('Bienvenido al servidor de riego automatizado');
+app.get("/", (_, res) => {
+  res.send("Bienvenido al servidor de riego automatizado");
 });
 
 app.listen(PORT, () => {
   console.log(`El servidor esta listo en el puerto ${PORT}`);
 });
-
