bd87d974e19582cddecd13e998bebbd6ce76d6b5
Jose Zambrana <eljosema505@gmail.com>
2025-01-19 15:44:27 -0400

    feat: HomePage done

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,13 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
---
+++ mobile/screens/HomeScreen.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { TextInput, Button, Text, HelperText } from "react-native-paper";
+import { useLinkTo } from "@react-navigation/native";
+
+interface LoginFormInputs {
+  email: string;
+  password: string;
+}
+
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Email inválido")
+      .required("El email es obligatorio"),
+    password: yup
+      .string()
+      .min(6, "La contraseña debe tener al menos 6 caracteres")
+      .required("La contraseña es obligatoria"),
+  })
+  .required();
+
+const LoginScreen = () => {
+  const linkTo = useLinkTo();
+
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/iot.png")} style={styles.image} />
+      <View style={styles.containerContent}>
+        <View style={styles.containerText}>
+          <Text style={styles.title}>
+            Descubre las ventajas de la tecnologia
+          </Text>
+        </View>
+
+        <View style={[styles.containerText, { width: 350 }]}>
+          <Text style={styles.subtitle}>
+            Controla tu riego cómodamente desde tu celular, cuidando tus plantas
+            en todo momento.
+          </Text>
+        </View>
+
+        <View style={styles.footer}>
+          <Button
+            mode="contained"
+            onPress={() => linkTo("/Login")}
+            style={styles.button}
+          >
+            Iniciar Sesión
+          </Button>
+          <Button
+            mode="contained"
+            onPress={() => linkTo("/Register")}
+            style={[styles.button, { backgroundColor: "#f5f5f5" }]}
+            labelStyle={{ color: "#000" }}
+          >
+            Crearse cuenta
+          </Button>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+    gap:20,
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  containerContent: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    gap: 10,
+  },
+  containerText: {
+    width: 300,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 15,
+    textAlign: "center",
+  },
+  footer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 5,
+  },
+  button: {
+    marginTop: 16,
+    backgroundColor: "#f5511e",
+    borderRadius: 7,
+  },
+});
+
+export default LoginScreen;
