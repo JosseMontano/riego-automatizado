debe042915d5a7d5f30ea9e34dba038cc6001930
JosseMontano <eljosema505@gmail.com>
2025-01-22 16:17:45 -0400

    feat: Return inbolivia time with build

--- server/build/models/sensorModel.js
+++ server/build/models/sensorModel.js
@@ -5,7 +5,14 @@ const sensorSchema = new mongoose_1.Schema({
     temperature: { type: Number, required: true },
     humidity: { type: Number, required: true },
     estado: { type: Boolean, required: true },
-    timestamp: { type: Date, default: Date.now },
+    timestamp: {
+        type: String,
+        default: () => {
+            const now = new Date();
+            const boliviaTime = new Date(now.getTime() - (4 * 3600000));
+            return boliviaTime.toTimeString().split(' ')[0];
+        },
+    },
 });
 const Sensor = (0, mongoose_1.model)('Sensor', sensorSchema);
 exports.default = Sensor;
